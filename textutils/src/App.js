07c827d17b077a00773a807b7ba33540fe008385
HBK0077 <hrishikeshbalakrishna07@gmail.com>
2023-10-16 22:53:33 +0530

    NavBar done

---
+++ textutils/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar title="TextUtils" aboutUs="About" home="Home"></Navbar>
+    </>
+  );
+}
+export default App;
