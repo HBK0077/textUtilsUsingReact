3b0f3c7989a3a6de31ff3fb327248f00cb0aac04
HBK0077 <hrishikeshbalakrishna07@gmail.com>
2023-10-17 18:33:44 +0530

    little extra features added with buttons

--- textutils/src/components/TextForm.js
+++ textutils/src/components/TextForm.js
@@ -1,6 +1,26 @@
 import React, {useState} from 'react'
 
 export default function TextForm(props) {
+    const [myStyle, setMyStyle] = useState({
+        color: "black",
+        backgroundColor: "white"
+    });
+    const [buttonText, setButtonText] = useState("Enable Dark Mode");
+    const toggleStyle = ()=>{
+        if(myStyle.color === "black"){
+            setMyStyle({
+                color: "white",
+                backgroundColor: "black"
+            });
+            setButtonText("Enable Light Mode");
+        }else{
+            setMyStyle({
+                color: "black",
+                backgroundColor: "white"
+            })
+            setButtonText("Enable Dark Mode");
+        }
+    }
     const handelUpperCase = () =>{
         //console.log("Please change me " + text);
         let newText = text.toUpperCase();
@@ -29,25 +49,15 @@ export default function TextForm(props) {
         }).join(" ");
         setText(newText);
     }
-    const [myStyle, setMyStyle] = useState({
-        color: "black",
-        backgroundColor: "white"
-    });
-    const [buttonText, setButtonText] = useState("Enable Dark Mode");
-    const toggleStyle = ()=>{
-        if(myStyle.color === "black"){
-            setMyStyle({
-                color: "white",
-                backgroundColor: "black"
-            });
-            setButtonText("Enable Light Mode");
-        }else{
-            setMyStyle({
-                color: "black",
-                backgroundColor: "white"
-            })
-            setButtonText("Enable Dark Mode");
-        }
+   
+    const handelCopyText = ()=>{
+        let copyText = document.getElementById("myBox")
+        copyText.select();
+        navigator.clipboard.writeText(copyText.value);
+    }
+    const handelExtraSpaces = ()=>{
+        let spacedText = text.replace(/\s+/g," ");
+        setText(spacedText);
     }
     const [text, setText] = useState("");
     // text="New Text" This is wrong way to change a state value
@@ -60,12 +70,17 @@ export default function TextForm(props) {
                     <div className="mb-3" >
                         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
                     </div>
+
+                    <button className="btn btn-primary mx-1" onClick={toggleStyle}>{buttonText}</button>
                     <button className="btn btn-primary mx-1" onClick={handelUpperCase}>Convert to UpperCase</button>
                     <button className="btn btn-primary mx-1" onClick={handelLowerCase}>Convert to LowerCase</button>
                     <button className="btn btn-primary mx-1" onClick={handelEmail}>Extract name from Email</button>
                     <button className="btn btn-primary mx-1" onClick={clearText}>Clear Text</button>
                     <button className="btn btn-primary mx-1" onClick={handelTitleCase}>Convert to TitleCase</button>
-                    <button className="btn btn-primary mx-1" onClick={toggleStyle}>{buttonText}</button>
+                    <button className="btn btn-primary my-2" onClick={handelCopyText}>Copy Text</button>
+                    <button className="btn btn-primary mx-1 my-2" onClick={handelExtraSpaces}>Remove Extra Spaces</button>
+
+                                        
                 </div>
                 <div className="container my-2">
                     <h2>Your text Summary</h2>
