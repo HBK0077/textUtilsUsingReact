cde86f9a4993d9bce764fd2460d68e64c598fb19
HBK0077 <hrishikeshbalakrishna07@gmail.com>
2023-10-17 19:16:08 +0530

    Dark mode functionality done

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -2,12 +2,24 @@ import './App.css';
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React ,{ useState } from 'react'
 
 
 function App() {
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode === "light"){
+      setMode("dark");
+      document.body.style.backgroundColor = "black";
+    }else{
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutUs="About" home="Home"></Navbar>
+      <Navbar title="TextUtils" aboutUs="About" home="Home" mode={mode} toggleMode = {toggleMode}></Navbar>
       <div className="container my-3">
         <TextForm heading="Enter the text to analyze"></TextForm>
         {/* <About title="About Us"></About> */}
